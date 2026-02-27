f5310b4c1bfc679b38dc336fb298a65d80c862c9
RK23-g <9b26vishwanathrkarthik@gmail.com>
2026-02-27 09:43:47 +0530

    welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+
+public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+
+        String appName = "PalindromeChecker App";
+        String version = "Version 1.0";
+
+
+        System.out.println("=================================");
+        System.out.println("Welcome to the " + appName + "!");
+        System.out.println(version);
+        System.out.println("=================================");
+
+
+        System.out.println("Application is ready to check palindromes.");
+    }
+}
